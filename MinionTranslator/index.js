098d974226265cfae39ca3090acaf661cdc6b6cd
Divyansh Shukla <divyansh.professional@gmail.com>
2021-01-19 15:07:21 +0530

    fix - Fixed no response from server error

--- MinionTranslator/index.js
+++ MinionTranslator/index.js
@@ -5,6 +5,10 @@ var outputAreaController = document.querySelector(".outputArea");
 var buttonController = document.querySelector(".translateButton");
 var textToSpeechButtonController = document.querySelector("#T2S");
 
+var defaultText = "";
+
+outputAreaController.value = defaultText;
+
 //function for text to speech
 function speak(text){
     var utterance = new SpeechSynthesisUtterance(text);
@@ -13,14 +17,32 @@ function speak(text){
     synth.speak(utterance);
 }
 
+//sleep function, for the value to appear in outputAreaTextBox
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve,ms));
+}
+
+//function that uses "sleep" function to delay
+
+async function delay()
+{
+    await sleep(3000);
+    if(outputAreaController.value == defaultText)
+    {
+        alert("The API limits the API calls to 5 per hour, which has exceeded, kindly try later.");
+    }
+}
+
 function makeURL(text){
     var URL = "https://api.funtranslations.com/translate/minion.json?text="+text;
     return URL;
 }
 
 buttonController.addEventListener('click', function(event){
-    var userInput = inputAreaController.value;
+    var userInput = inputAreaController.value;    
     fetch(makeURL(userInput)).then(response => response.json()).then(data => outputAreaController.value = data.contents.translated);
+    defaultText = outputAreaController.value;
+    delay(); // separate function because await only works for async functions
 });
 
 textToSpeechButtonController.addEventListener('click', function(event){
